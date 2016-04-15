c26bb6f76601c8c61b695a9c75e5c710787b49fe
Valery Kalashnikov <kalashnikov.valeri@gmail.com>
2016-04-15 08:35:43 +0600

    Modify performance tests for different types of caches.
    Now sharded cache is too slow compared with simple cache. But it works faster in concurrent mode rather than in single-goroutine mode.

--- cache/sharded_cache_performance_test.go
+++ cache/sharded_cache_performance_test.go
@@ -4,8 +4,8 @@ import (
         "testing"
         )
 
-func BenchmarkCacheSetStringTest(b *testing.B) {
-  cache := NewCache()
+func BenchmarkShardedCacheSetStringTest(b *testing.B) {
+  cache := NewShardedCache()
   presentTime = &PresentTime{0}
   // run the Set function b.N times
   for n := 0; n < b.N; n++ {
@@ -13,8 +13,9 @@ func BenchmarkCacheSetStringTest(b *testing.B) {
   }
 }
 
-func BenchmarkCacheSetListTest(b *testing.B) {
-  cache := NewCache()
+
+func BenchmarkShardedCacheSetListTest(b *testing.B) {
+  cache := NewShardedCache()
   presentTime = &PresentTime{0}
   value := [2]string{"Penn", "Teller"}
   // run the Set function b.N times
@@ -23,8 +24,8 @@ func BenchmarkCacheSetListTest(b *testing.B) {
   }
 }
 
-func BenchmarkCacheSetMapTest(b *testing.B) {
-  cache := NewCache()
+func BenchmarkShardedCacheSetMapTest(b *testing.B) {
+  cache := NewShardedCache()
   presentTime = &PresentTime{0}
   value := map[string]int{"rsc": 3711, "r": 2138}
   // run the Set function b.N times
@@ -33,8 +34,8 @@ func BenchmarkCacheSetMapTest(b *testing.B) {
   }
 }
 
-func BenchmarkCacheParallel(b *testing.B) {
-  cache := NewCache()
+func BenchmarkShardedCacheParallel(b *testing.B) {
+  cache := NewShardedCache()
   b.RunParallel(func(pb *testing.PB) {
     presentTime = &PresentTime{0}
     for pb.Next() {
@@ -44,8 +45,8 @@ func BenchmarkCacheParallel(b *testing.B) {
 }
 
 
-func BenchmarkCacheGetValueTest(b *testing.B) {
-  cache := NewCache()
+func BenchmarkShardedCacheGetValueTest(b *testing.B) {
+  cache := NewShardedCache()
   presentTime = &PresentTime{0}
   value := "value"
   cache.Set("key", value, presentTime)
@@ -55,8 +56,8 @@ func BenchmarkCacheGetValueTest(b *testing.B) {
 }
 
 
-func BenchmarkCacheUpdateValueTest(b *testing.B) {
-  cache := NewCache()
+func BenchmarkShardedCacheUpdateValueTest(b *testing.B) {
+  cache := NewShardedCache()
   presentTime = &PresentTime{0}
   value := "New value"
   cache.Set("key", value, presentTime)
@@ -65,8 +66,8 @@ func BenchmarkCacheUpdateValueTest(b *testing.B) {
   }
 }
 
-func BenchmarkCacheDeleteValueTest(b *testing.B) {
-  cache := NewCache()
+func BenchmarkShardedCacheDeleteValueTest(b *testing.B) {
+  cache := NewShardedCache()
   presentTime = &PresentTime{0}
   for i := 0; i < 10000; i++ {
     cache.Set("key", i, presentTime)
@@ -76,8 +77,8 @@ func BenchmarkCacheDeleteValueTest(b *testing.B) {
   }
 }
 
-func BenchmarkCacheKeysValueTest(b *testing.B) {
-  cache := NewCache()
+func BenchmarkShardedCacheKeysValueTest(b *testing.B) {
+  cache := NewShardedCache()
   presentTime = &PresentTime{0}
   for i := 0; i < 10000; i++ {
     cache.Set("key", i, presentTime)
